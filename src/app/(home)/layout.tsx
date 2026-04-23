7878010ea6902446a97ffab661a61e9279a356b1
devaeterne <info@safaksariyildiz.com>
2026-04-23 11:34:06 +0200

    first. load

--- src/app/(home)/layout.tsx
+++ src/app/(home)/layout.tsx
@@ -2,12 +2,11 @@ import HeaderTwo from "@/components/header/header-two";
 import BackToTop from "@/components/back-to-top";
 import MainProvider from "@/components/provider/main-provider";
 import FooterTwo from "@/components/footer/footer-two";
-import { Analytics } from "@vercel/analytics/next"
+
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <MainProvider>
-      <Analytics />
       {/* header area start */}
       <HeaderTwo />
       {/* header area end */}
